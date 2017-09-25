da8c16d7816aba007d6a82cb9fa3a86de64f31e0
RRCCDataLab <adam.forland@rrcc.edu>
2017-09-25 17:41:55 -0600

    Changed 3.14 to pi

--- Projects/normal_hats/graphing.py
+++ Projects/normal_hats/graphing.py
@@ -5,12 +5,13 @@ from sympy import lambdify
 import matplotlib.pyplot as mpl
 
 t = symbols('t')
-x = 1/sqrt(3.14) * exp(-1/2 * t**2)
+x = 1/sqrt(pi) * exp(-1/2 * t**2)
 lam_x = lambdify(t, x, modules=['numpy'])
 
 x_vals = linspace(-3, 3, 100)
 y_vals = lam_x(x_vals)
 
 mpl.plot(x_vals, y_vals)
-mpl.ylabel("Speed")
+mpl.xlabel("x values")
+mpl.ylabel("Density")
 mpl.show()
