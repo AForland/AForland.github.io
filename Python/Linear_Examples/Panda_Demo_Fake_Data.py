41085845b8fe31e3b585976730aa330a99be5b4a
RRCCDataLab <adam.forland@rrcc.edu>
2017-10-08 09:39:51 -0600

    removed extra files

--- Python/Linear_Examples/Panda_Demo_Fake_Data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-Data_Frame = pd.read_csv('/home/adam/Documents/Python/10to6Data.csv',names=['Initials', 'User ip', 'Time in', 'Time out','Average Mbps','Phone','ip visited','Path'])
-
-print(Data_Frame)
